287fe908852c87ea973ec1cdcc765d02eefad764
Admin <Admin@DESKTOP-EBOERVM>
2016-11-09 14:05:32 +0100

    First Commit Norbert Wołowiec

--- WaspOS/src/core/Main.java
+++ WaspOS/src/core/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Wasp OS");
 		System.out.println("Grzegorz Osak");
+		System.out.println("Norbert Wo³owiec");
 	}
 }
