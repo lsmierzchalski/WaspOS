dd2626ec95c423d22b0ca5cd30759336fbd68707
Łukasz <ukasz@DESKTOP-4N4EMMD>
2016-11-09 14:02:37 +0100

    Commit - Łukasz Wolniak - First commit MASTER

--- WaspOS/src/core/Main.java
+++ WaspOS/src/core/Main.java
@@ -2,7 +2,8 @@ package core;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Wasp OS");
+		System.out.println("WaspOS");
 		System.out.println("Grzegorz Osak");
+		System.out.println("£ukasz Wolniak");
 	}
 }
