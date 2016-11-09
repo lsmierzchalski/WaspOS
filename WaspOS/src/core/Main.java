79846a49f3c391c72a0030a3b7f6928271abfb37
Lukasz <lsmierzchalski@gmail.com>
2016-11-09 14:08:01 +0100

    coment luki

--- WaspOS/src/core/Main.java
+++ WaspOS/src/core/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Wasp OS");
 		System.out.println("Grzegorz Osak");
-		System.out.println("£ukasz Œmierzchalski");
+		System.out.println("£ukasz Œmierzchalski 222222");
 	}
 }
