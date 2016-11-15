991b219e5f973d6f7a62c52c9c9b5c965982438e
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2016-11-15 13:25:10 +0100

    Próba commitu Jarek

--- WaspOS/src/core/Main.java
+++ WaspOS/src/core/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Wasp OS");
 		System.out.println("Grzegorz Osak new branch");
+		System.out.println("Jarek Wieczorek new branch");
 	}
 }
