c9a7bd86305e2739da8462c8c985f408164c393f
Admin <Admin@DESKTOP-EBOERVM>
2016-11-23 14:17:54 +0100

    Test

---
+++ WaspOS/src/syncMethod/TestPCB.java
@@ -0,0 +1,12 @@
+package syncMethod;
+import java.util.*;
+
+public class TestPCB {
+	public boolean blocked;
+	public TestPCB(){
+		this.blocked=false;
+	}
+	public void pokaStan(){
+		System.out.println(blocked);
+	}
+}
