9c7b23d39c2cd4f56069ec6cab661f18a57867e4
Łukasz <ukasz@DESKTOP-4N4EMMD>
2016-11-23 14:33:15 +0100

    New package

--- WaspOS/src/core/Main.java
+++ WaspOS/src/core/Main.java
@@ -3,7 +3,6 @@ package core;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("WaspOS");
-		System.out.println("Grzegorz Osak");
 		System.out.println("£ukasz Wolniak");
 	}
 }
