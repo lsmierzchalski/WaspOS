c74e9fd473a1e7e99ec29cff99ed30a5ee615805
Lukasz <lukasz.maksymilian.wolniak@gmail.com>
2016-12-09 18:50:41 +0100

    dużo zmian - za dużo by pisać

--- WaspOS/src/processorManager/ProcessorManager.java
+++ WaspOS/src/processorManager/ProcessorManager.java
@@ -2,29 +2,42 @@ package processorManager;
 import ProcessesManagment.Proces; 
 import ProcessesManagment.ProcessesManagment;
 
-// Rejestr D jako licznik wykonanych rozkazow - DO ZROBIENIA PRZEZ JARKA!
-// EMPTY - Pusty proces do wrzucenia na poczatku do RUNNING i NEXTTRY!
+// Pole howLongWaiting + getter/setter - GRACJAN + getter whenCameToList (co proces dodany on ma sobie to inkrementowaæ co 1)!
 // + wywolanie INTERPRETERA!
-// + POSTARZANIE PROCESOW gdy getCommantCounter() % 3!
+// + POSTARZANIE PROCESOW - jesli howLongWaitnig = 3, dynamiczny proces + 1, howLongWaiting = 0!
+// + Gdy schodzi z RUNNING proces, dynamicznyPiorytet = bazowy!
 // + MOJE GETTERY : Co jest akurat w RUNNING i NEXTTRY + MOZE TEKSTY GDY WYOWLUJE FUNKCJE POSZCZEGOLNE(?)!
+// + zabecpieczenie przed wartoscia NULL/0 w RUNNING i NEXTTRY!
 // ? WSZYSTKO W RUN ?
 // ? JAREK ZWROCI MI ZERO TO ZNOW DZIALAM ?
 // !!! PROTEZY OD JARKA I GRACJANA !!!
+// PROGRAMY Z PLIKU!!!
+// SHELL!!
+
+/**
+ * Proces bezczynnoœci, bym móg³ porównywaæ procesy z listy z polem RUNNING i NEXTTRY.
+ * 
+ * @author £UKASZ WOLNIAK
+ */
+Proces idleProcess = new Proces();
+idleProcess.CreateProcess(0,idleProcess, 0);
+idleProcess.SetBasePririty(0);
+idleProcess.SetCurrentPririty(0);
+idleProcess.SetWhenCameToList(0);
+
 /**
  * Pole przechowuj¹ce aktualnie uruchomiony proces.
  * 
  * @author £UKASZ WOLNIAK
  */
-public static Proces RUNNING; 
+public static Proces RUNNING = idleProcess; 
 
 /**
  * Pole przechowuj¹ce kandydata na kolejny proces do uruchomienia.
- * Przed pobraniem procesu z NEXTTRY i wrzuceniu go do RUNNING, sprawdzane jest czy w liœcie procesów nie ma procesu o wy¿szym piorytecie. 
- * Je¿eli takowy jest, to on zostaje wrzucony do pola RUNNING, a pole NEXTTRY siê nie zmienia.
  * 
  * @author £UKASZ WOLNIAK
  */
-public static Proces NEXTTRY;
+public static Proces NEXTTRY = idleProcess;
 
 /**
  * Funkcja sprawdzaj¹ca, czy w polu RUNNING jest na pewno proces o najwy¿szym piorytecie z dostepnych tych na liscie procesow + FCFS.
@@ -36,13 +49,16 @@ private void checkRUNNING(){
 	for(Proces processFromList : processesList){
 		if((processFromList.GetCurrentPriority() >= RUNNING.GetCurrentPriority()) && processFromList.GetState()==1){
 			if(processFromList.GetCurrentPriority() == RUNNING.GetCurrentPriority()){
-				if(processFormList.GetNumberOfAdd() < RUNNING.processFormList.GetNumberOfAdd()){
+				if(processFormList.WhenCameToList() < RUNNING.processFormList.WhenCameToList()){
+					RUNNING = SetCurrentPriority(GetBasePriority());
 					RUNNING = processFromList;
 					RUNNING = RUNNING.SetState(2);
 				}
 			}
 			else{
-			RUNNING = processFromList;
+				RUNNING = SetCurrentPriority(RUNNING.GetBasePriority());
+				RUNNING = processFromList;
+				RUNNING = RUNNING.SetState(2);
 			}
 		}
 	}
@@ -59,11 +75,13 @@ private void checkNEXTTRY(){
 		if((processFromList.GetCurrentPriority() >= NEXTTRY.GetCurrentPriority()) && (processFromList.GetState()==1  || processFromList.GetState()==3)){
 			if(RUNNING!=processFromList){
 				if(processFromList.GetCurrentPriority() == NEXTTRY.GetCurrentPriority()){
-					if(processFormList.GetNumberOfAdd() < NEXTTRY.processFormList.GetNumberOfAdd()){
+					if(processFormList.WhenCameToList() < NEXTTRY.processFormList.WhenCameToList()){
+						NEXTTRY = SetCurrentPriority(NEXTTRY.GetBasePriority());
 						NEXTTRY = processFromList;
 					}
 				}
 				else{
+					NEXTTRY = SetCurrentPriority(NEXTTRY.GetBasePriority());
 					NEXTTRY = processFromList;
 				}
 			}
@@ -73,38 +91,84 @@ private void checkNEXTTRY(){
 
 /**
  * Funkcja rozwi¹zuj¹ca problem g³odzenia piorytetów.
- * Je¿eli zmienna sendProcessToRunningCounter jest podzielna bez reszty przez 3 (czyl i co 3 wyslane procesy), to obecny piorytet procesu o najmniejszym piorytecie jest zwiêkszany o 1.
+ * Co rozkaz zwiêkszany jest piorytet procesu o najmniejszym piorytecie z listy.
  * Wywolane zostaje rownie¿ sprawdzenie pola najpierw RUNNING (czy czasem zwiekszony proces nie ma wiekszego piorytetu niz ten w RUNNING) oraz NEXTTRY (jezeli nie ma wiekszy od tego w RUNNING, to moze ma wiekszy chociaz od tego w NEXTTRY).
+ * ZALOZENIE : 2 procesy nie moga w tej samej chwili wejsc na liste (wiec wyiberamy tlyko jeden glodujacy a nie grupe glodujacych).
+ *
  * @author £UKASZ WOLNIAK
  */
 private void checkStarving(){
-		private Proces weakestProcess = processesList.get(1);
-		for(Proces processFromList : processesList){
-			if(processFromList.GetCurrentPriority() <= weakestProcess.GetCurrentPriority()){
-				if(processFormList.GetNumberOfAdd() == weakestProcess.processFormList.GetNumberOfAdd()){
-					if(processFormList.GetNumberOfAdd() < NEXTTRY.processFormList.GetNumberOfAdd()){
-					weakestProcess = processFromList;
-					}
-			}
-				else{
-					weakestProcess = processFromList;
-				}
-		for(Proces processFromList : processesList){
-			if(processFromList.GetID() == weakestProcess.GetID()){
-				processFromList.SetCurrentPriority(processFromList.GetCurrentPiority()+1);
-			}
-		}
-		checkRUNNING();
-		checkNEXTTRY();
-	} 
-	}	
+
+}
+/**
+ * Funkcja zwiêksza pole howLongWaiting - je¿eli proces nie jest aktywny, zwiêkszamy jego pole howLongWaiting o 1.
+ * 
+ * @author £UKASZ WOLNIAK
+ */
+private void changeWaiting(){
+	
 }
 
 /**
- * 
+ * Funkcja "postarzaj¹ca" procesy.
+ * Je¿eli jakiœ proces czeka 3 rozkazy (howLongWaiting) i wszedl najpozniej na liste to jego obecny piorytet jest zwiêkszany o 1 i howLongWaiting jest zerowane.
+ *  Na koncu sprawdzamy pola NEXTTRY i RUNNING;
+ *  
+ * @author £UKASZ WOLNIAK
  */
 private void checkAging(){
-	
+	private Proces weakestProcess;
+	for(Proces processFromList : processesList){
+		if(processFromList.GetHowLongWaiting==3){
+			if(processFromList.GetWhenCameToList > weakestProcess.GetWhenCameToList()){
+				weakestProcess = processFromList;
+			}
+		}
+	}
+	for(Proces processFromListCheck : processesList){
+		if(processFromListCheck.GetID() == weakestProcess.GetID()){
+			processFromListCheck.SetCurrentPriority(processFormListCheck()+1);
+		}
+	}
+	compareNEXTTRYandRUNNING();
+}
+
+/**
+ *  Funkcja porównuj¹ca pola NEXTTRY i RUNNING.
+ *  Je¿eli proces w NEXTTRY : nie jest zablokowany, ma wiêkszy piorytet od procesu w RUNNING lub ma rowny piorytet, ale wczeœniej wszed³ na listê, to zamieniane jest pole RUNNING, a pole NEXTTRY odpowiednio uzupe³niane. 
+ * 	je¿eli nie, do podmiany pola RUNNING wyszukiwany jest proces z listy RUNNING.
+ *  @author £UKASZ WOLNIAK
+ */
+private void comapreNEXTTRYandRUNNING(){
+	//SPRAWDZENIE CZY NA PEWNO W NEXTTRY JEST PROCES O NAJWYZCZYM PIORYTECIE
+	checkNEXTTRY();
+	if(NEXTTRY.GetBlocked()==false){
+		if(NEXTTRY.GetCurrentPriority()>= RUNNING.GetCurrentPriority()){
+			if(NEXTTRY.GetCurrentPriority() == RUNNING.GetCurrentPriority()){
+				if(NEXTTRY.GetWhenCameToList() < RUNNING.GetWhenCameToList()){
+					RUNNING.SetCurrentPriority(RUNNING.GetBasePriority());
+					RUNNING = NEXTTRY;
+					NEXTTRY = idleProcess;
+					checkNEXTTRY();
+				}
+				else{
+					checkRUNNING();
+				}
+			}
+			else{
+				RUNNING.SetCurrentPriority(RUNNING.GetBasePriority());
+				RUNNING = NEXTTRY;
+				NEXTTRY = idleProcess;
+				checkNEXTTRY();
+			}
+		}
+		else{
+			checkRUNNING();
+		}
+	}
+	else{
+		checkRUNNING();
+	}
 }
 
 /**
@@ -113,18 +177,13 @@ private void checkAging(){
  * @author £UKASZ WOLNIAK
  */
 public class ProcessorManager {
-	//SPRAWDZAMY CZY PROCES W RUNNING JEST ZABLOKOWANY LUB ZAKONCZONY
-	IF(RUNNING.GetState()==4 || RUNNING.GetState()==3){
-		//SPRAWDZENIE CZY NA PEWNO W NEXTTRY JEST PROCES O NAJWYZCZYM PIORYTECIE
-		checkNEXTTRY();
-		//JEZELI PROCES NIE JEST ZABLOKOWANY TO WRZUCAMY GO DO RUNNING I WRZUCAMY NOWY DO NEXTTRY, A JEZELI NIE TO Z LISTY SZUKAMY NOWY DO RUNNINGU (BO W RUNNING SPRAWDZAMY CZY NIE JEST BLOKED)
-		if(NEXTTRY.GetBlocked()==false){
-			RUNNING = NEXTTRY;
-			checkNEXTTRY();
+		//Jezeli proces zosta³ zablokowany lub zakonczony, pod RUNNING podstawiamy proces bezczynnoœci.
+		if(RUNNING.GetState()==3 || RUNNING.GetState==4){
+			RUNNING.SetCurrentPriority(RUNNING.GetBasePriority()); 
+			RUNNING = idleProcess;
 		}
-		else{
-			checkRUNNING();
-		}
+		//JEZELI PROCES NIE JEST ZABLOKOWANY, PIORYTET MA WIEKSZY, MOMENT WEJSCIA NA LISTE MNIEJSZY - WRZUCAMY GO DO RUNNING, NEXTTRY ODPOWIEDNIO ZMIENIAMY - funkcja compareRUNNINGandNEXTTRY
+		compareNEXTTRYandRUNNING();
+		// SPRAWDZAMY CZY JAKIS GLODUJE, 
 		checkStarving();
 	}
-}
