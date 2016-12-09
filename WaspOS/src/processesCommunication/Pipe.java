c314c8b1ff8ec37cc5fde62b30df7f87d90b9a9a
Grzegorz Osak <g4rhold@gmail.com>
2016-12-09 14:47:35 +0100

    Główna klasa połączenia.

---
+++ WaspOS/src/processesCommunication/Pipe.java
@@ -0,0 +1,12 @@
+package processesCommunication;
+
+/**
+ * G³ówna klasa wymiany danych pomiêdzy procesami.
+ * 
+ * @author Gosak.
+ */
+public class Pipe {
+	public Pipe() {
+		
+	}
+}
---
+++ WaspOS/src/processesCommunication/Process.java
@@ -0,0 +1,5 @@
+package processesCommunication;
+
+public class Process {
+
+}
