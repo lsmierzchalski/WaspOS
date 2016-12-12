ccd1fce62e57ce6f8fb9fcf1105e3c223bb51fa9
Nokem61 <nokem61@gmail.com>
2016-12-12 12:07:12 +0100

    Nie wiem do końca co robie w tym Gicie kurcze

--- WaspOS/src/core/Main.java
+++ WaspOS/src/core/Main.java
@@ -2,8 +2,6 @@ package core;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Wasp OS");
-		System.out.println("Grzegorz Osak");
-		System.out.println("Norbert Wo³owiec");
+		
 	}
 }
