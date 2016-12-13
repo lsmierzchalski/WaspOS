d768a6aca7904b03cb52662eecc6f8b7f15ec0c7
Grzegorz Osak <g4rhold@gmail.com>
2016-12-14 00:42:30 +0100

    repush

--- WaspOS/src/commandInterpreter/Interpreter.java
+++ WaspOS/src/commandInterpreter/Interpreter.java
@@ -123,7 +123,7 @@ public class Interpreter {
 
 	public int RUN(Process RUNNING) {
 		if(ProcessorManager.RUNNING.GetState() == 4)
-			return -1;
+			return -1; //TODO KURWAAAAA
 		ProcessorManager.RUNNING.printInformations();
 		// Put to Box a PCB from current Process
 		PCBbox = RUNNING.GetPCB();
